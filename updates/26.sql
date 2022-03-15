f21731579a9dc261ea4fc507f5c71b8d70fdd90b
Matt <matt@mjman.net>
2022-03-15 18:56:40 +1100

    correct sql

--- updates/26.sql
+++ updates/26.sql
@@ -1,3 +1,3 @@
-CREATE TABLE users_groups AS SELECT * FROM groups;
+CREATE TABLE users_groups AS SELECT * FROM `groups`;
 CREATE TABLE users_users_groups AS SELECT * FROM groups_users;
 ALTER TABLE `users_users_groups` CHANGE COLUMN `group_id` `users_group_id` INT NULL DEFAULT NULL;
